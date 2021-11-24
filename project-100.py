2d57a5ef6339b57470d574a090d5b702c12a7222
Keshu123324 <74176491+Keshu123324@users.noreply.github.com>
2021-11-24 19:57:47 +0530

    Add files via upload

---
+++ project-100.py
@@ -0,0 +1,14 @@
+class ATM(object):
+    def__init__(self,Card,Pin):
+        self.Card=Card
+        self.Pin=Pin
+
+    def cashWithdrawl(self):
+        print("Cash withdrawl in progress.......completed")
+
+    def balanceEnquiry(self):
+        print("Balance enquiry")
+
+    mybank=("1234","1234")
+    print(mybank.cashWithdraw())
+    print(mybank.balanceEnquiry())
